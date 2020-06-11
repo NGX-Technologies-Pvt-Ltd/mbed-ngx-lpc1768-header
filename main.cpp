3ec83d5a439dc01a236559ec50fb1987247fea16
vivek <vivek@ngxtechnologies.com>
2020-06-11 15:12:35 +0530

    refactor(Code readability): led given proper name

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 #include "mbed.h"
 
-DigitalOut myled(LED1);
-DigitalOut myled2(LED2);
+DigitalOut testLed(LED1);
+DigitalOut usbLed(LED2);
 
 int main()
 {
     while(1) {
-        myled = 0;
-        myled2 = 0;
+        testLed = 0;
+        usbLed = 0;
         wait_us(50000);
-        myled = 1;
-        myled2 = 1;
+        testLed = 1;
+        usbLed = 1;
         wait_us(50000);
     }
 }
